5808588cbd60c4e62c80a7a522ee937a6ec6b98d
MariusChesovan <marius.chesovan@evozon.com>
2018-04-13 10:36:35 +0300

    meine 60 and 90 tage

---
+++ pages/academy_m90_page.py
@@ -0,0 +1,117 @@
+from tools.webdriver_commands import WebdriverCommands
+from tools.webdriver_commands import ElementNotFound
+
+TRAINING_1 = {'css_selector': "div.post-536 > a", 'description': 'Teamaufbau Training (Fortgeschritten)'}
+TRAINING_2 = {'css_selector': "div.post-539 > a", 'description': 'Style Party Training - werde zum Experten'}
+TRAINING_3 = {'css_selector': "div.post-541 > a", 'description': 'Einführung: Leadership Training'}
+BREADCRUMBS_1 = {'css_selector': "a[class='trail-begin']", 'description': 'breadcrumbs home'}
+MARK_COMPLETE_BUTTON = {'css_selector': "#learndash_mark_complete_button", 'description': 'mark as complete button'}
+NEXT_LESSON = {'css_selector': "a[class='next-link']", 'description': 'next link'}
+PREVIOUS_LESSON = {'css_selector': "a[class='prev-link']", 'description': 'previous link'}
+
+
+
+class AcademyMeine90Page:
+
+    def __init__(self, driver):
+        self.page = WebdriverCommands(driver)
+
+    def save_screen(self):
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90.png')
+
+    def click_training_1(self):
+        self.page.move_to_element('lessons_list')
+        self.page.click_element(TRAINING_1['css_selector'], TRAINING_1['description'])
+
+    def click_training_2(self):
+        self.page.move_to_element('lessons_list')
+        self.page.click_element(TRAINING_2['css_selector'], TRAINING_2['description'])
+
+    def click_training_3(self):
+        self.page.move_to_element('lessons_list')
+        self.page.click_element(TRAINING_3['css_selector'], TRAINING_3['description'])
+
+    def click_next_link(self):
+        try:
+            self.page.move_to_element('learndash_next_prev_link')
+            self.page.click_element(NEXT_LESSON['css_selector'], NEXT_LESSON['description'],1)
+        except ElementNotFound:
+            print('{} is not present on this page'.format(NEXT_LESSON['description']))
+            pass
+
+    def click_previous_link(self):
+        try:
+            self.page.move_to_element('learndash_next_prev_link')
+            self.page.click_element(PREVIOUS_LESSON['css_selector'], PREVIOUS_LESSON['description'], 1)
+        except ElementNotFound:
+            print('{} is not present on this page'.format(PREVIOUS_LESSON['description']))
+            pass
+
+    def click_mark_complete_button(self):
+        try:
+            self.page.move_to_element('learndash_next_prev_link')
+            self.page.click_element(MARK_COMPLETE_BUTTON['css_selector'], MARK_COMPLETE_BUTTON['description'],1)
+        except ElementNotFound:
+            self.click_next_link()
+            pass
+
+    def click_breadcrumbs_first(self):
+        self.page.click_element(BREADCRUMBS_1['css_selector'], BREADCRUMBS_1['description'],1)
+
+    def complete_first_training(self):
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90_before_training1.png')
+        self.click_training_1()
+        try:
+            self.click_mark_complete_button()
+            self.click_breadcrumbs_first()
+        except:
+            self.click_breadcrumbs_first()
+            pass
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90_after_training1.png')
+
+    def complete_second_training(self):
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90_before_training2.png')
+        self.click_training_2()
+        try:
+            self.click_mark_complete_button()
+            self.click_breadcrumbs_first()
+        except:
+            self.click_breadcrumbs_first()
+            pass
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90_after_training2.png')
+
+    def complete_third_training(self):
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90_before_training3.png')
+        self.click_training_3()
+        try:
+            self.click_mark_complete_button()
+            # self.click_breadcrumbs_first()
+        except:
+            # self.click_breadcrumbs_first()
+            pass
+        self.page.save_screenshot('C:/Users/mariuschesovan/Desktop/pat_screenshots/meine90_after_training3.png')
+
+    def complete_course(self):
+        trainings = []
+        lessons = self.page.get_web_elements('#lessons_list > div', '.notcompleted')
+        for lesson in lessons:
+            trainings.append(lesson.text)
+            # print(lesson.text)
+        for x in range(len(trainings)):
+            if trainings[x] == 'Teamaufbau Training (Fortgeschritten)':
+                try:
+                    self.complete_first_training()
+                except:
+                    print('cazut la if 1')
+            elif trainings[x] == 'Style Party Training - werde zum Experten':
+                try:
+                    self.complete_second_training()
+                except:
+                    print('cazut la if 2')
+            elif trainings[x] == 'Einführung: Leadership Training':
+                try:
+                    self.complete_third_training()
+                except:
+                    print('cazut la if 3')
+            x += 1
+        print("Meine 90 training finished")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,12 +1,14 @@
 import unittest
 
 from pages.academy_m30_page import AcademyMeine30Page
+from pages.academy_m60_page import AcademyMeine60Page
+from pages.academy_m90_page import AcademyMeine90Page
 from pages.page_example import PageExample
 from pages.sc_registration import ScRegistationPage
 from pages.academy_ospt_page import AcademyOsptPage
 from tools.parametrized_test_case import ParametrizedTestCase
 
-email = "antionette.wells@pwp-academy.com"
+email = "carla.lyons@pwp-academy.com"
 password = "123qwe"
 text_search = "Fascination"
 text_search1 = "Earrings"
@@ -21,47 +23,65 @@ class TestExample(ParametrizedTestCase):
         driver.get('https://staging.pippajean.com/de/walter/')
         page_example = PageExample(driver)
         sc_registration = ScRegistationPage(driver)
-        sc_registration.register_sc()
+        # sc_registration.register_sc()
 
         #
-        # page_example.click_sign_in_button()
-        # page_example.enter_email(email)
-        # page_example.enter_password(password)
-        # page_example.click_login_button()
-
-        # tema scenariul 1 todo
-        # page_example.enter_code(text_search)
-        # page_example.click_search_button()
-        # page_example.get_search_result(text_search1)
-
-        # meine 30 todo
-        driver.get('https://staging-academy.pippajean.com/courses/meine-ersten-30-tage/')
-        academy_m30_page = AcademyMeine30Page(driver)
-        academy_m30_page.save_screen()
-        academy_m30_page.complete_course()
+        page_example.click_sign_in_button()
+        page_example.enter_email(email)
+        page_example.enter_password(password)
+        page_example.click_login_button()
 
         # ospm training todo
-        # driver.get('https://staging-academy.pippajean.com/courses/online-style-party-training/')
-        # academy_ospt_page = AcademyOsptPage(driver)
-        # academy_ospt_page.click_mache_dieses_training_button()
-        # academy_ospt_page.click_lesson1()
-        # academy_ospt_page.click_mark_complete_button()
-        # academy_ospt_page.click_mark_complete_button()
-        # academy_ospt_page.click_quiz_on_lesson3()
-        # academy_ospt_page.click_start_quiz_button()
-        # academy_ospt_page.lesson_quiz(3)
+        driver.get('https://staging-academy.pippajean.com/courses/online-style-party-training/')
+        academy_ospt_page = AcademyOsptPage(driver)
+        academy_ospt_page.click_mache_dieses_training_button()
+        academy_ospt_page.click_lesson1()
+        academy_ospt_page.click_mark_complete_button()
+        academy_ospt_page.click_mark_complete_button()
+        academy_ospt_page.click_quiz_on_lesson3()
+        academy_ospt_page.click_start_quiz_button()
+        academy_ospt_page.lesson_quiz(3)
 
 
 
-    @unittest.skip('Reason')
+    @unittest.skip('pus in comment pana termion restul')
     def test_example2(self):
         '''
             self.driver is a parameter inherited from ParametrizedTestCase
         '''
         driver = self.driver
-        driver.get('https://www.myprotein.ro')
+        driver.get('https://staging.pippajean.com/de/walter/')
         page_example = PageExample(driver)
-        page_example.click()
+        sc_registration = ScRegistationPage(driver)
+        # sc_registration.register_sc()
+
+        #
+        page_example.click_sign_in_button()
+        page_example.enter_email(email)
+        page_example.enter_password(password)
+        page_example.click_login_button()
+
+        # meine 30 did
+        # pus in comment pana termion restul
+        driver.get('https://staging-academy.pippajean.com/courses/meine-ersten-30-tage/')
+        academy_m30_page = AcademyMeine30Page(driver)
+        academy_m30_page.save_screen()
+        academy_m30_page.complete_course()
+
+        # # meine 60 did
+        # # pus in comment pana termion restul
+        driver.get('https://staging-academy.pippajean.com/courses/meine-ersten-60-tage/')
+        academy_m60_page = AcademyMeine60Page(driver)
+        academy_m60_page.save_screen()
+        academy_m60_page.complete_course()
+
+        # # # meine 90 did
+        # # # pus in comment pana termion restul
+        driver.get('https://staging-academy.pippajean.com/courses/meine-ersten-90-tage/')
+        academy_m90_page = AcademyMeine90Page(driver)
+        academy_m90_page.save_screen()
+        academy_m90_page.complete_course()
+
 
     @unittest.skip('Reason2')
     def test_example3(self):
@@ -69,10 +89,22 @@ class TestExample(ParametrizedTestCase):
             self.driver is a parameter inherited from ParametrizedTestCase
         '''
         driver = self.driver
-        driver.get('https://www.myprotein.ro')
-        x = PageExample(driver)
-        x.click()
-        x.verify_title()
+        driver.get('https://staging.pippajean.com/de/walter/')
+        page_example = PageExample(driver)
+        sc_registration = ScRegistationPage(driver)
+        # sc_registration.register_sc()
+
+        #
+        page_example.click_sign_in_button()
+        page_example.enter_email(email)
+        page_example.enter_password(password)
+        page_example.click_login_button()
+
+
+        # tema scenariul 1 todo
+        # page_example.enter_code(text_search)
+        # page_example.click_search_button()
+        # page_example.get_search_result(text_search1)
 
 
 
